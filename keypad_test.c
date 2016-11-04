af2154cb45958d8cff25da11ac5f1cc475fbbe8a
dandz12 <dandiaz@pdx.edu>
2016-11-04 13:53:52 -0700

    Add files via upload
    This file is still testing the keypad with an LED but is written for our final design.

--- keypad_test.c
+++ keypad_test.c
@@ -9,49 +9,66 @@
 #include <util/delay.h>
 
 void blink(int);
+int getKey(void);
 
 int main(void)
 {
-	int num = 0;
-	DDRB |= 1 << PINB0;				//set port B as output
-	DDRD |= 0b11000000;				//set port D as output
-	
-
-	PORTD = 0b00110000;
+	int key = 0;
+	int passCode[4];				//sets and array of int's 4 elements long, this will be our pass code
+	DDRB |= 1 << PINB0;				//set port B as output for LED
+	DDRC |= 0xF0;					//set port C as output
+	PORTC = 0x0F;					//active pull up resistor
 	
     while (1) 
     {
-		if(PIND != 0b11000000)		//detect if a key is pressed
-		{
+		if(PINC != 0xF0)
+			blink(getKey());
+    }
+}
+
+//this function will flip bits on port C and return the value of the key pressed
+int getKey(void)
+{
+			int num = 0;
+			
+			//flip bits and set num
 			_delay_ms(5);			//delay just in case for debouncing
-			num = PIND;				//assign num the digit pressed
-			
-			DDRD ^= 0xF0;			//set d7/d6 as inputs and d4/d5 as outputs, this is XORing to flip bits
+			num = PINC;				//assign num the digit pressed
+			DDRC ^= 0xFF;			//XOR and set to flip port direction
 			_delay_ms(1);
-			PORTD ^= 0xF0;			//set pins high, again using XOR set
+			PORTC ^= 0xFF;			//set pins high, again using XOR set
 			_delay_ms(1);
-			
-			num |= PIND;			//or and set number to what PinD is reading
+			num |= PINC;			//or and set number to what PinD is reading
 			
 			//determine the button pressed and call blink LED function
-			if(num == 0b10100000)
-				blink(5);
-			if(num == 0b10010000)
-				blink(4);
-			if(num == 0b01100000)
-				blink(2);
-			if(num == 0b01010000)
-				blink(1);
+			if(num == 0x88)
+				num = 1;
+			if(num == 0x48)
+				num = 2;
+			if(num == 0x28)
+				num = 3;
+			if(num == 0x84)
+				num = 4;
+			if(num == 0x44)
+				num = 5;
+			if(num == 0x24)
+				num = 6;
+			if(num == 0x82)
+				num = 7;
+			if(num == 0x42)
+				num = 8;
+			if(num == 0x22)
+				num = 9;
+			if(num == 0x41)
+				num = 0;
 				
 			//reset everything to its initial state for the next button press
-			num = 0;
-			DDRD ^= 0xF0;
+			DDRC ^= 0xFF;
 			_delay_ms(1);
-			PORTD ^= 0xF0;
-			_delay_ms(5);			 
+			PORTC ^= 0xFF;
+			_delay_ms(5);			 	
 			
-		}
-    }
+			return num;
 }
 
 //simply take in number and blink that amount
