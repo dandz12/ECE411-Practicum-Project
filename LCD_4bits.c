23e52191eda3ca365eb37bdd2a27b06a5309cb73
trankhiemha <tranha@pdx.edu>
2016-11-17 16:16:10 -0800

    Update LCD code
    uploaded the older one last time :D

--- LCD_4bits.c
+++ LCD_4bits.c
@@ -2,82 +2,79 @@
 #include <util/delay.h>
 
 #define ctrl PORTD
-#define en 2							// enable signal
-#define rs 0							// register select signal
+#define en 2                         // enable signal
+#define rs 0                     // register select signal
 
-void lcd_ini(void);						// Initialize LCD
-
-void LCD_cmd(unsigned char cmd);		// Send 4 bits command (send 2 times to have 8 bits command)
-void LCD_write(unsigned char data);		// Send 4 bits data (send 2 times to have 8 bits command)
-
-void dis_cmd(char);						// Send full 8 bits command
-void dis_data(char);					// Send full 8 bits data
-
-void LCD_write_string(unsigned char *str);		// Display a string
-
-
-
+void LCD_cmd(unsigned char cmd);
+void lcd_ini(void);
+void LCD_write(unsigned char data);
+void dis_cmd(char);
+void dis_data(char);
+void LCD_write_string(unsigned char *str);
 
 void main()
 {
-	DDRD=0xFF;                                  // Set port D as output                                                                     
-	lcd_ini();									// initialization of LCD
-	LCD_write_string("hello");			// function to print string on LCD
-	dis_cmd(0xC0);								// move to second line
-	LCD_write_string("world");			// function to print string on LCD
+	DDRD=0xFF;                                  // setting the port c                                                                       // setting for port D
+	
+	lcd_ini();                                 // initialization of LCD
+	_delay_ms(30);                        // delay of 50 mili seconds
+	dis_data('b');
+
 	while(1);
 }
 
 void lcd_ini(void)
 {
 	dis_cmd(0x02); // to initialize LCD in 4-bit mode.
-	dis_cmd(0x28); // to initialize LCD in 2 lines, 5X7 dots and 4bit mode.
-	dis_cmd(0x01); // clear LCD
-	dis_cmd(0x0E); // cursor ON
-	dis_cmd(0x80); // —8 go to first line and –0 is for 0th position
-	_delay_ms(30); // final delay to make sure that LCD is ready
+	_delay_ms(1);
+	dis_cmd(0x28); //to initialize LCD in 2 lines, 5X7 dots and 4bit mode.
+	_delay_ms(1);
+	dis_cmd(0x01);                                 // clear LCD
+	_delay_ms(1);
+	dis_cmd(0x0E);                        // cursor ON
+	_delay_ms(1);
+	dis_cmd(0x80);                     // â€”8 go to first line and â€“0 is for 0th position
+	_delay_ms(1);
 	return;
 }
 
-void dis_cmd(char cmd_value)			
+void dis_cmd(char cmd_value)
 {
 	char cmd_value1;
-	cmd_value1 = cmd_value & 0xF0;          //Mask the lower 4 bits
-	LCD_cmd(cmd_value1);					//Send the higher 4 bits
-	cmd_value1 = ((cmd_value<<4) & 0xF0);   //Shift left 4 bits
-	LCD_cmd(cmd_value1);					//Send the lower 4 bits
-	_delay_ms(1);
+	cmd_value1 = cmd_value & 0xF0;          //mask lower nibble
+	//because PA4-PA7 pins are used.
+	LCD_cmd(cmd_value1);               // send to LCD
+	cmd_value1 = ((cmd_value<<4) & 0xF0);     //shift 4-bit and
+	//mask
+	LCD_cmd(cmd_value1);               // send to LCD
 }
-
 void dis_data(char data_value)
 {
 	char data_value1;
-	data_value1=data_value & 0xF0;			//Mask the lower 4 bits
-	LCD_write(data_value1);					//Send the higher 4 bits
-	data_value1=((data_value<<4) & 0xF0);	//Shift left 4 bits
-	LCD_write(data_value1);					//Send the lower 4 bits
-	_delay_ms(1);
+	data_value1=data_value & 0xF0;
+	LCD_write(data_value1);
+	data_value1=((data_value<<4) & 0xF0);
+	LCD_write(data_value1);
 }
 
-
-void LCD_cmd(unsigned char cmd)				
+void LCD_cmd(unsigned char cmd)
 {
-	ctrl=cmd;								//output the 4 bit command
-	ctrl&=~(1<<rs);							//resistor select to send instructor
-	ctrl|=(1<<en);							//enable high
+	ctrl=cmd;
+	ctrl&=~(1<<rs);
+	ctrl|=(1<<en);
 	_delay_ms(1);
-	ctrl&=~(1<<en);							//enable low
+	ctrl&=~(1<<en);
 	_delay_ms(40);
 	return;
 }
 
 void LCD_write(unsigned char data)
 {
-	ctrl= data;								//output the 4 bit data
-	ctrl|=(1<<rs);							//resistor select to send data
-	ctrl|=(1<<en);							//enable high
+	ctrl= data;
+	ctrl|=(1<<rs);
+	ctrl|=(1<<en);
 	_delay_ms(1);
-	ctrl&=~(1<<en);							//enable low
+	ctrl&=~(1<<en);
 	_delay_ms(40);
 	return ;
 }
@@ -87,7 +84,7 @@ void LCD_write_string(unsigned char *str)             //store address value of t
 	int i=0;
 	while(str[i]!='\0')                               // loop will go on till the NULL character in the string
 	{
-		dis_data(str[i]);							  // sending data on LCD byte by byte
+		dis_data(str[i]);                            // sending data on LCD byte by byte
 		i++;
 	}
 	return;
